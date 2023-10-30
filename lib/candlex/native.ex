02c1bb6fd46decc302bed8f7e398eab5318c1521
Gonzalo <456459+grzuy@users.noreply.github.com>
2023-10-30 17:51:23 -0300

    build: leave windows out for now as xla

--- lib/candlex/native.ex
+++ lib/candlex/native.ex
@@ -18,8 +18,6 @@ defmodule Candlex.Native do
       "aarch64-apple-darwin",
       "aarch64-unknown-linux-gnu",
       "x86_64-apple-darwin",
-      "x86_64-pc-windows-gnu",
-      "x86_64-pc-windows-msvc",
       "x86_64-unknown-linux-gnu"
     ]
 
