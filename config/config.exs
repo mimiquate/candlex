d644fe3eb61d75b228804bc060a5c863a6f5215f
Gonzalo <456459+grzuy@users.noreply.github.com>
2023-10-31 13:38:28 -0300

    wip build: NIF binaries targeting CUDA

--- config/config.exs
+++ config/config.exs
@@ -1,17 +1,3 @@
 import Config
 
-enable_cuda =
-  case System.get_env("CUDA") do
-    nil -> System.find_executable("nvcc") && System.find_executable("nvidia-smi")
-    "false" -> false
-    _ -> true
-  end
-
-crate_features =
-  if enable_cuda do
-    [:cuda]
-  else
-    []
-  end
-
-config :candlex, crate_features: crate_features
+config :candlex, use_cuda: (System.get_env("NATIVE_TARGET") == "cuda")
