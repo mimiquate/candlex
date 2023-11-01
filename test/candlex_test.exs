9ddde1d41f01ee6fbdfd1b2e8cd7a63190db4119
Gonzalo <456459+grzuy@users.noreply.github.com>
2023-11-01 14:16:17 -0300

    test: all/1

--- test/candlex_test.exs
+++ test/candlex_test.exs
@@ -2143,6 +2143,20 @@ defmodule CandlexTest do
       )
     end
 
+    test "all" do
+      t([0, 1, 2])
+      |> Nx.all()
+      |> assert_equal(t(0))
+
+      # t([[-1, 0, 1], [2, 3, 4]], names: [:x, :y])
+      # |> Nx.all(axes: [:x])
+      # |> assert_equal(t([1, 0, 1]))
+
+      # t([[-1, 0, 1], [2, 3, 4]], names: [:x, :y])
+      # |> Nx.all(axes: [:y])
+      # |> assert_equal(t([0, 1]))
+    end
+
     if Candlex.Backend.cuda_available?() do
       test "different devices" do
         t([1, 2, 3], backend: {Candlex.Backend, device: :cpu})
