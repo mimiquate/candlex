bbe4bdcdb7d54472f24241b185f4f6a8aa85961b
Gonzalo <456459+grzuy@users.noreply.github.com>
2023-11-06 11:06:22 -0300

    style: fix compile warnings back

--- mix.exs
+++ mix.exs
@@ -32,8 +32,8 @@ defmodule Candlex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:nx, "~> 0.6"},
-      # {:nx, git: "https://github.com/elixir-nx/nx", sparse: "nx"},
+      # {:nx, "~> 0.6"},
+      {:nx, git: "https://github.com/elixir-nx/nx", sparse: "nx"},
       {:rustler_precompiled, "~> 0.7.0"},
 
       # Optional
