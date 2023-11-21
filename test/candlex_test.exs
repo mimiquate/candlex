0ed2122ef3571f4ea6e3fe39030ed84ec2f96375
Gonzalo <456459+grzuy@users.noreply.github.com>
2023-11-21 14:23:17 -0300

    Update test/candlex_test.exs

--- test/candlex_test.exs
+++ test/candlex_test.exs
@@ -1511,7 +1511,7 @@ defmodule CandlexTest do
 
       Nx.iota({1, 1, 10, 10})
       |> Nx.conv(Nx.iota({2, 1, 3, 3}), strides: 2, padding: :same)
-      |> assert_close(
+      |> assert_equal(
         t([
           [
             [
